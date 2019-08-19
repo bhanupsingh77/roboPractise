eac989ebbfade5a0dbf613693ec97567147b6a6f
BHANU PRATAP SINGH <allstarbhanupsingh@gmail.com>
2019-08-19 22:51:37 +0530

    Updates

---
+++ precache-manifest.05e30bf852e25806adbbfe06e78db22b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17501fb91c2c3f0006141ca5bf645e97",
+    "url": "/roboPractise/index.html"
+  },
+  {
+    "revision": "4f3cd7349731c1600c8d",
+    "url": "/roboPractise/static/css/main.05f30b57.chunk.css"
+  },
+  {
+    "revision": "f7d6e0430bef7b477bb2",
+    "url": "/roboPractise/static/js/2.5a7af95d.chunk.js"
+  },
+  {
+    "revision": "4f3cd7349731c1600c8d",
+    "url": "/roboPractise/static/js/main.e96d3123.chunk.js"
+  },
+  {
+    "revision": "cf0ec8824aa814a3fa14",
+    "url": "/roboPractise/static/js/runtime~main.7a50cfed.js"
+  }
+]);
